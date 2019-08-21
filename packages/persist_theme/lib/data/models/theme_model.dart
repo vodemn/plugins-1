4ad4ad353c7af0feab758c520963f50da015912a
vodemn <44135514+vodemn@users.noreply.github.com>
2019-08-21 17:22:21 +0300

    Update theme_model.dart

--- packages/persist_theme/lib/data/models/theme_model.dart
+++ packages/persist_theme/lib/data/models/theme_model.dart
@@ -77,7 +77,9 @@ class ThemeModel extends ChangeNotifier {
     }
     switch (type) {
       case ThemeType.light:
-        return customLightTheme ?? ThemeData.light().copyWith();
+        return customLightTheme ?? ThemeData.light().copyWith(
+          primaryColorDark: Colors.white,
+        );
       case ThemeType.dark:
         return customDarkTheme ??
             ThemeData.dark().copyWith(
